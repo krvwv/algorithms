7138394deb0822172e289aa6ab19c5a811458676
krvwv <pkarwo@wp.pl>
2020-03-24 20:39:29 +0100

    Added quick sort

--- src/com/algorithms/Main.java
+++ src/com/algorithms/Main.java
@@ -1,12 +1,20 @@
 package com.algorithms;
 import java.util.Arrays;
-
+import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        int[] array={7,4,8,224,54,15,88,23,62,21,1,6};
-        System.out.println(Arrays.toString(array));
+        Random random=new Random();
+        int[] array=new int[100000];
+        int[] array2;
+        for (int i=0;i<array.length;i++)
+        {
+            array[i]=random.nextInt(1000);
+        }
+        array2=Arrays.copyOf(array,array.length);
+
 	    BubbleSort.bubble(array);
-        System.out.println(Arrays.toString(array));
-    }
+	    QuickSort.quick(array2,0, array2.length-1);
+
+}
 }
--- src/com/algorithms/QuickSort.java
+++ src/com/algorithms/QuickSort.java
@@ -1,7 +1,31 @@
 package com.algorithms;
 
 public class QuickSort {
-    public static void quick(){
-
+    public static void quick(int[] array, int p, int q){
+        int pivot=array[(p+q)/ 2];
+        int i,j,temp;
+        i=p;
+        j=q;
+        while(i<=j){
+            while(array[i]<pivot){
+                i++;
+            }
+            while(array[j]>pivot){
+                j--;
+            }
+            if(i<=j){
+                temp=array[i];
+                array[i]=array[j];
+                array[j]=temp;
+                i++;
+                j--;
+            }
+        }
+        if(j>p){
+            quick(array,p,j);
+        }
+        if(i<q){
+            quick(array,i,q);
+        }
     }
 }
