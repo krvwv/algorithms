08de5f7dce85875161152ba13b986ba62c24762c
krvwv <pkarwo@wp.pl>
2020-03-24 18:18:18 +0100

    Added bubble sort

--- src/com/algorithms/BubbleSort.java
+++ src/com/algorithms/BubbleSort.java
@@ -1,4 +1,16 @@
 package com.algorithms;
 
 public class BubbleSort {
+    public static void bubble(Object[] array){
+        Object temp;
+        for (int i=array.length-1;i>0;i--){
+            for(int j=0;j<i;j++){
+                if(((Comparable) array[j]).compareTo(array[j + 1]) > 0){
+                    temp=array[j+1];
+                    array[j+1]=array[j];
+                    array[j]=temp;
+                }
+            }
+        }
+    }
 }
--- src/com/algorithms/Main.java
+++ src/com/algorithms/Main.java
@@ -1,8 +1,12 @@
 package com.algorithms;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Integer[] array={7,4,8,224,54,15,88,23,62,21,1,6};
+        System.out.println(Arrays.toString(array));
+	    BubbleSort.bubble(array);
+        System.out.println(Arrays.toString(array));
     }
 }
